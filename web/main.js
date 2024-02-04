b1b2aeead27efd1f59404b43a879064e21c57111
Chad Collins <chadrcollins91@gmail.com>
2024-02-04 15:43:34 -0500

    logging build input

--- web/main.js
+++ web/main.js
@@ -1,33 +1,41 @@
-const { invoke } = window.__TAURI__.tauri;
+const {invoke} = window.__TAURI__.tauri;
 
 let greetInputEl;
 let greetMsgEl;
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsgEl.textContent = await invoke("greet", {name: greetInputEl.value});
+}
+
+async function build() {
+    let input = document.querySelector("#sourceInput").value;
+    let info = await invoke("build", {name: "default", input: input});
+
+    console.log(info);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  let statusBar = document.querySelector("#statusBar");
+    let statusBar = document.querySelector("#statusBar");
 
-  document.querySelector("#openBtn").addEventListener("click", (e) => {
-    e.preventDefault();
-    statusBar.innerText = "Open";
-  });
+    document.querySelector("#openBtn").addEventListener("click", (e) => {
+        e.preventDefault();
+        statusBar.innerText = "Open";
+    });
 
-  document.querySelector("#newBtn").addEventListener("click", (e) => {
-    e.preventDefault();
-    statusBar.innerText = "New";
-  });
+    document.querySelector("#newBtn").addEventListener("click", (e) => {
+        e.preventDefault();
+        statusBar.innerText = "New";
+    });
 
-  document.querySelector("#buildBtn").addEventListener("click", (e) => {
-    e.preventDefault();
-    statusBar.innerText = "Build";
-  });
+    document.querySelector("#buildBtn").addEventListener("click", (e) => {
+        e.preventDefault();
+        statusBar.innerText = "Build";
+        build();
+    });
 
-  document.querySelector("#saveBtn").addEventListener("click", (e) => {
-    e.preventDefault();
-    statusBar.innerText = "Save";
-  });
+    document.querySelector("#saveBtn").addEventListener("click", (e) => {
+        e.preventDefault();
+        statusBar.innerText = "Save";
+    });
 });
