85af541ca9318a4b04912e084722739768896bfa
Chad Collins <chadrcollins91@gmail.com>
2024-02-11 15:13:39 -0500

    Added commands for execution state

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod context;
 mod compile;
+mod context;
 
+use crate::compile::extract_annotation_parts;
+use crate::context::ViewerContext;
 use garnish_annotations_collector::{Collector, Sink, TokenBlock};
-use garnish_data::{SimpleRuntimeData, symbol_value};
-use garnish_lang_compiler::{build_with_data, InstructionMetadata, lex, LexerToken, parse, ParseResult, TokenType};
+use garnish_data::{symbol_value, SimpleRuntimeData};
+use garnish_lang_compiler::{
+    build_with_data, lex, parse, InstructionMetadata, LexerToken, ParseResult, TokenType,
+};
 use garnish_lang_runtime::runtime_impls::SimpleGarnishRuntime;
 use garnish_traits::{GarnishLangRuntimeData, GarnishRuntime};
 use log::{debug, error};
 use serde::{Deserialize, Serialize};
-use crate::compile::extract_annotation_parts;
-use crate::context::ViewerContext;
+use std::collections::HashMap;
+use std::sync::Mutex;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct BuildInfo {
     all_lexer_tokens: Vec<LexerToken>,
+    source_tokens: HashMap<String, Vec<LexerToken>>,
     context: ViewerContext,
     runtime_data: SimpleRuntimeData,
 }
@@ -31,22 +36,49 @@ struct ExpressionBuildInfo {
     instruction_metadata: Vec<InstructionMetadata>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct SourceInfo {
+    name: String,
+    text: String,
+}
+
+struct AppState {
+    base_build: Mutex<Option<BuildInfo>>,
+}
+
 #[tauri::command]
 fn build(name: Option<&str>, input: Option<&str>) -> Result<BuildInfo, String> {
     match (name, input) {
         (Some(name), Some(input)) => build_input(name, input),
-        _ => Err("Name and input required".to_string())
+        _ => Err("Name and input required".to_string()),
     }
 }
 
 fn build_input(name: &str, input: &str) -> Result<BuildInfo, String> {
-
     let mut runtime = SimpleGarnishRuntime::new(SimpleRuntimeData::new());
     let mut context = ViewerContext::new();
 
-    let collector: Collector = Collector::new(vec![
-        Sink::new("@Def").until_token(TokenType::Subexpression),
-    ]);
+    let source_tokens = build_input_with_context(name, input, &mut runtime, &mut context)?;
+
+    let mut sources = HashMap::new();
+    sources.insert(name.to_string(), source_tokens.clone());
+
+    Ok(BuildInfo {
+        all_lexer_tokens: source_tokens,
+        source_tokens: sources,
+        runtime_data: runtime.get_data().clone(),
+        context,
+    })
+}
+
+fn build_input_with_context(
+    name: &str,
+    input: &str,
+    runtime: &mut SimpleGarnishRuntime<SimpleRuntimeData>,
+    context: &mut ViewerContext,
+) -> Result<Vec<LexerToken>, String> {
+    let collector: Collector =
+        Collector::new(vec![Sink::new("@Def").until_token(TokenType::Subexpression)]);
 
     let tokens = lex(&input)?;
     let blocks: Vec<TokenBlock> = collector.collect_tokens(&tokens)?;
@@ -56,7 +88,7 @@ fn build_input(name: &str, input: &str) -> Result<BuildInfo, String> {
         .partition(|b| b.annotation_text().is_empty());
 
     let mut def_metadata =
-        handle_def_annotations(annotation_blocks, &mut runtime, &mut context, &name.to_string())?;
+        handle_def_annotations(annotation_blocks, runtime, context, &name.to_string())?;
 
     context.metadata_mut().append(&mut def_metadata);
 
@@ -94,14 +126,13 @@ fn build_input(name: &str, input: &str) -> Result<BuildInfo, String> {
 
     context.metadata_mut().push(root_metadata);
 
-    runtime.get_data_mut().get_data_mut().insert_expression(index, symbol_value(&name));
+    runtime
+        .get_data_mut()
+        .get_data_mut()
+        .insert_expression(index, symbol_value(&name));
     context.insert_expression(name, index);
 
-    Ok(BuildInfo {
-        all_lexer_tokens: tokens,
-        runtime_data: runtime.get_data().clone(),
-        context,
-    })
+    Ok(tokens)
 }
 
 fn handle_def_annotations(
@@ -143,12 +174,14 @@ fn handle_def_annotations(
             text: source,
             tokens: Vec::from(annotation_parts.expression),
             parse_result: parsed,
-            instruction_metadata: instruction_data
-
+            instruction_metadata: instruction_data,
         });
 
         debug!("Found method: {}", name);
-        runtime.get_data_mut().get_data_mut().insert_expression(expression_index, symbol_value(&annotation_parts.name_token.get_text()));
+        runtime.get_data_mut().get_data_mut().insert_expression(
+            expression_index,
+            symbol_value(&annotation_parts.name_token.get_text()),
+        );
         context.insert_expression(annotation_parts.name_token.get_text(), expression_index);
     }
 
@@ -174,8 +207,40 @@ fn build_and_get_parameters(
     Ok((parsed, instruction_data, index))
 }
 
+#[tauri::command]
+fn initialize_execution(
+    sources: Vec<SourceInfo>,
+    state: tauri::State<AppState>,
+) -> Result<(), String> {
+    let mut source_tokens = HashMap::new();
+    let mut runtime = SimpleGarnishRuntime::new(SimpleRuntimeData::new());
+    let mut context = ViewerContext::new();
+
+    for source in sources {
+        source_tokens.insert(
+            source.name.clone(),
+            build_input_with_context(&source.name, &source.text, &mut runtime, &mut context)?,
+        );
+    }
+
+    *state.base_build.lock().unwrap() = Some(BuildInfo {
+        source_tokens,
+        all_lexer_tokens: vec![],
+        runtime_data: runtime.get_data().clone(),
+        context,
+    });
+
+    Ok(())
+}
+
+#[tauri::command]
+fn get_execution_build(state: tauri::State<AppState>) -> Option<BuildInfo> {
+    state.base_build.lock().unwrap().clone()
+}
+
 fn main() {
     tauri::Builder::default()
+        .manage(AppState { base_build: Mutex::new(None) })
         .invoke_handler(tauri::generate_handler![build])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
