7064943fc15945b246df41fdd2bfe84fa84264a1
Chad Collins <chadrcollins91@gmail.com>
2024-02-20 09:54:51 -0500

    Implemented PageNav

--- vue_src/components/shared/PageNav.vue
+++ vue_src/components/shared/PageNav.vue
@@ -1,38 +1,78 @@
 <script setup lang="ts">
 import {computed} from "vue";
+import {exists} from "../../utils/general";
+import {clamp} from "../../utils/math";
+
+defineEmits(["toEnd", "toStart", "previousClicked", "nextClicked", "itemClicked"]);
 
 const props = withDefaults(defineProps<{
-  title?: string,
   pageCount: number,
+  limit?: number,
+  selected?: number,
+  showToStart?: boolean,
+  showToEnd?: boolean,
 }>(), {
-  title: null,
-  pageCount: 0
+  pageCount: 0,
+  selected: 0,
+  showToStart: false,
+  showToEnd: false,
 });
+
+const truePageLimit = computed(() => props.limit || props.pageCount);
+const maxVisibleStart = computed(() => truePageLimit.value >= props.pageCount ? 0 : props.pageCount - truePageLimit.value);
+const visibleStart = computed(() => clamp(props.selected - (truePageLimit.value/2), 0, maxVisibleStart.value));
+const visibleEnd = computed(() => Math.min(visibleStart.value + truePageLimit.value, props.pageCount));
+
+const previousVisible = computed(() => exists(props.limit));
+const previousEnabled = computed(() => visibleStart.value > 0);
+const nextVisible = computed(() => exists(props.limit));
+const nextEnabled = computed(() => visibleEnd.value < props.pageCount);
 
 const pageWindow = computed(() => {
   let pages = [];
-  for (let i = 0; i < props.pageCount; i++) {
+  for (let i = visibleStart.value; i < visibleEnd.value; i++) {
     pages.push(i);
   }
 
   return pages;
 })
+
 </script>
 
 <template>
 <nav>
-  <h6 v-if="title">
-    {{ props.title }}
-  </h6>
   <ol>
-    <li v-for="p in pageWindow">
-      {{ p }}
+    <li v-if="showToStart"
+        :class="{disabled: !previousEnabled}"
+        @click="$emit('toStart')">
+      <span>⮜⮜</span>
+    </li>
+    <li v-if="previousVisible"
+        :class="{disabled: !previousEnabled}"
+        @click="$emit('previousClicked')">
+      <span>⮜</span>
+    </li>
+    <li v-for="p in pageWindow" @click="$emit('itemClicked', p)">
+      <span>{{ p }}</span>
+    </li>
+    <li v-if="nextVisible"
+        :class="{disabled: !nextEnabled}"
+        @click="$emit('nextClicked')">
+      <span>⮞</span>
+    </li>
+    <li v-if="nextVisible"
+        :class="{disabled: !nextEnabled}"
+        @click="$emit('toEnd')">
+      <span>⮞⮞</span>
     </li>
   </ol>
 </nav>
 </template>
 
 <style scoped>
+h6 {
+  margin-bottom: 1rem;
+}
 ol {
   list-style: none;
   display: flex;
@@ -45,6 +85,25 @@ ol > li {
   background-color: var(--edit_color);
   text-align: center;
   margin-right: .5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+ol > li:hover {
+  background-color: var(--highlight_color);
+}
+
+ol > li:active {
+  background-color: var(--active_color);
+}
+
+ol > li.disabled {
+  background-color: var(--accent_color);
+}
+
+ol > li > span {
+  flex-grow: 1;
 }
 
 ol > li:last-child {
--- vue_src/stories/PageNav.stories.ts
+++ vue_src/stories/PageNav.stories.ts
@@ -2,7 +2,7 @@ import type {Meta, StoryObj} from '@storybook/vue3';
 
 import PageNav from "../components/shared/PageNav.vue";
 import {within} from "@storybook/testing-library";
-import {expect} from "@storybook/jest";
+import {expect} from "@storybook/test";
 
 const meta = {
     title: 'Components/PageNav',
@@ -22,22 +22,98 @@ export const Basic: Story = {
 
         for (let i = 0; i < 10; i++) {
             await expect(
-                canvas.getByText(`${i}`)
+                canvas.queryByText(`${i}`)
             ).toBeInTheDocument()
         }
     }
 };
 
-export const WithTitle: Story = {
+export const DisplayLimit: Story = {
     args: {
-        title: "My Nav",
-        pageCount: 10
+        pageCount: 20,
+        limit: 10
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+
+        for (let i = 0; i < 10; i++) {
+            await expect(
+                canvas.queryByText(`${i}`)
+            ).toBeInTheDocument()
+        }
+
+        for (let i = 10; i < 20; i++) {
+            await expect(
+                canvas.queryByText(`${i}`)
+            ).toBeNull()
+        }
+
+        await expect(
+            canvas.queryByText("⮜")
+        ).toBeInTheDocument()
+
+        await expect(
+            canvas.queryByText("⮞")
+        ).toBeInTheDocument()
+    }
+};
+
+export const Selected: Story = {
+    args: {
+        pageCount: 20,
+        limit: 10,
+        selected: 13
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+
+        for (let i = 8; i < 18; i++) {
+            await expect(
+                canvas.queryByText(`${i}`)
+            ).toBeInTheDocument()
+        }
+
+        for (let i = 0; i < 8; i++) {
+            await expect(
+                canvas.queryByText(`${i}`)
+            ).toBeNull()
+        }
+
+        await expect(
+            canvas.queryByText("18")
+        ).toBeNull()
+
+        await expect(
+            canvas.queryByText("19")
+        ).toBeNull()
+
+        await expect(
+            canvas.queryByText("⮜")
+        ).toBeInTheDocument()
+
+        await expect(
+            canvas.queryByText("⮞")
+        ).toBeInTheDocument()
+    }
+};
+
+export const ShowStartEnd: Story = {
+    args: {
+        pageCount: 20,
+        limit: 10,
+        selected: 13,
+        showToStart: true,
+        showToEnd: true,
     },
     play: async ({ canvasElement }) => {
         const canvas = within(canvasElement);
 
         await expect(
-            canvas.getByText("My Nav")
+            canvas.queryByText("⮜⮜")
+        ).toBeInTheDocument()
+
+        await expect(
+            canvas.queryByText("⮞⮞")
         ).toBeInTheDocument()
     }
 };
